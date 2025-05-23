5286adcc6478d87879a8840128fcad5af90e8141
Lucas Aquino <lc.aquinodeoliveira@gmail.com>
2025-05-23 00:36:57 -0300

    feat: added new route for forms and first project

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\AuthController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get("oi/{texto}", function ($text) {
-    echo "Oi, " . $text;
-});
-
+// Button Homepage
 Route::get("/", [HomeController::class, "index"]);
+
+// Working with Forms
+Route::get("/form", [FormController::class, "showForm"])->name("form.show");
+Route::post("/form", [FormController::class, "submitForm"])->name("form.submit");
+
+// First project: Login Auth
+Route::get("/login", [AuthController::class, "showLoginForm"])->name("login.show");
+Route::post("/login", [AuthController::class, "processLogin"])->name("login.process");
+
+// First project: Login Auth - Admin area
+Route::post("/admin", [AuthController::class, "adminArea"])->name("admin.area");
