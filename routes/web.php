2f27a13fed6f78ccf46752461a79b0916861f879
Lucas Aquino <lc.aquinodeoliveira@gmail.com>
2025-05-22 21:33:13 -0300

    feat: added new route for the homepage

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get("oi/{texto}", function ($text) {
     echo "Oi, " . $text;
 });
+
+Route::get("/", [HomeController::class, "index"]);
