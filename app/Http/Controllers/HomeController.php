19afa45111d5b75f377e723beb2030ce6c0af1e2
Lucas Aquino <lc.aquinodeoliveira@gmail.com>
2025-05-23 00:36:06 -0300

    docs: changed comment language

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller {
     public function index() {
-        $dataHoraAtual = date("d H:i");  // Dia e Hora
-        return view("home", compact('dataHoraAtual'));  // Envia para a view.
+        $dataHoraAtual = date("d H:i");  // Shows Date and Hour
+        return view("home", compact('dataHoraAtual'));  // Sends to View
     }
 }
