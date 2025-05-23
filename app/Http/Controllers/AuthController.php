4dfef7274ff6101ae825252444079f36074c40d3
Lucas Aquino <lc.aquinodeoliveira@gmail.com>
2025-05-23 00:40:42 -0300

    feat: controller preparation for first project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller {
+    public function showLoginForm() {
+        //fill
+    }
+
+    public function processLogin() {
+        //fill
+    }
+
+    public function adminArea() {
+        //fill
+    }
+}
